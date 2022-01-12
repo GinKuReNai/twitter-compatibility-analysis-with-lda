ad3caf729ae135b6b559df84258a3888388d0bab
GinKuReNai <akito121215@gmail.com>
2022-01-12 09:04:34 +0900

    前処理を修正 & 評価指標の算出を追加

--- lda/gensim_lda.py
+++ lda/gensim_lda.py
@@ -1,24 +1,29 @@
 # GensimでLDAトピックモデリング
 from collections import defaultdict
-from gensim import models, corpora
+from gensim import models, corpora # Gensim Library
 import unicodedata # 正規化用
 import neologdn # 正規化用
-import glob
+import glob # パスの一括取得
 import sys
-import MeCab
-import re
+import MeCab # 形態素解析
+import re # 正規表現
+import pandas # CSV I/O
 
 # ------------------------------------------------------
 
 # 探索パスのパターン
-path_pattern = '../aozorabunko_text/cards/*/files/*_ruby_*/*_utf-8_revised.txt'
+#aozora_path_pattern = '../aozorabunko_text/cards/*/files/*_ruby_*/*_utf-8_revised.txt'
+path_patterns = ['/home/s192c1058/data/*.txt', '/home/s192c1058/data/yomiuri/*.txt']
 # 入力ファイル名
-inputfile = glob.glob(path_pattern, recursive=False)
+inputfile = []
+for path in path_patterns:
+  inputfile += glob.glob(path, recursive=False)
 # MeCabの初期化
-mc = MeCab.Tagger("/usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd")
+#mc = MeCab.Tagger("/usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd")
+mc = MeCab.Tagger("-d ../../../usr/local/lib/mecab/dic/mecab-ipadic-neologd")
 mc.parse('')
 
-# ストップワードリスト + 正規表現パターン
+# ストップワードリスト
 stopwords_file = open('Japanese-revised.txt', 'r')
 stopwords = [line.strip() for line in stopwords_file]
 stopwords = [ss for ss in stopwords if not ss==u'']
@@ -34,7 +39,11 @@ def get_tokens(sent):
   sent_normalized = neologdn.normalize(sent)
   sent_normalized = unicodedata.normalize('NFKC', sent_normalized)
 
-  node = mc.parseToNode(sent)
+  # 数字と桁区切り文字を全て0に変換
+  sent_normalized = re.sub(r'(\d)([,.])(\d+)', r'\1\3', sent_normalized)
+  sent_normalized = re.sub(r'\d+', '0', sent_normalized)
+
+  node = mc.parseToNode(sent_normalized)
   tokens = []
   # 固有名詞を含めて代名詞を除く
   while node:
@@ -64,6 +73,28 @@ def del_stopwords(sent, stopwords=[]):
 
   return revised_texts
 
+# LDA結果のトピックを出力するメソッド
+def lda_print_topics(lda):
+  for i in range(N):
+    print("\n")
+    print("="*80)
+    print("TOPIC {0}\n".format(i))
+    topic = lda.show_topic(i)
+    for t in topic:
+        print("{0:20s}{1}".format(t[0], t[1]))
+
+# モデルの評価指標を算出するメソッド
+def compute_metrics(model, texts, corpus, dct):
+  # Compute Perplexity(モデルの予測精度の評価指標)
+  perplexity = lda.log_perplexity(corpus)
+
+  # Compute Coherence Score（トピックの品質を測る評価指標）
+  coherence_model_lda = models.CoherenceModel(model=model, texts=texts, dictionary=dct, coherence='u_mass')
+  coherence_lda = coherence_model_lda.get_coherence()
+
+  return perplexity, coherence_lda
+
+
 # -------------------------------------------------
           
 # 形態素解析した結果をtextsに追加
@@ -73,10 +104,10 @@ for i in range(len(inputfile)):
       texts.append(get_tokens(line))
 
 # ストップワードを削除
-texts = del_stopwords(texts, slothlib_stopwords)
+texts = del_stopwords(texts, stopwords)
 
 # 形態素解析の結果を出力
-with open('neologd_output.txt','w') as f:
+with open('neologd_outcome.txt','w') as f:
   f.write(str(texts))
 
 # 単語の頻度の辞書
@@ -85,30 +116,50 @@ for text in texts:
   for token in text:
     frequency[token] += 1
 
+
 # n回以上出現する単語に限定
 n = 10
 processed_corpus = [[token for token in text if frequency[token] > n] for text in texts]
 
+
 # 単語ごとにIDを割り振る
 dictionary = corpora.Dictionary(processed_corpus)
-dictionary.filter_extremes(no_below=1, no_above=0.15)
-#print(dictionary.token2id)
+dictionary.filter_extremes(no_above=0.3)
+dictionary.save_as_text('/home/s192c1058/models/dict.txt')
 
 # BoWを作成
 bow_corpus = [dictionary.doc2bow(text) for text in processed_corpus]
-#print(bow_corpus)
 
 # Tfidfを作成
 tfidf = models.TfidfModel(bow_corpus)
 corpus_tfidf = tfidf[bow_corpus]
 
-# LDAを作成
-N = 10 # トピック数
-corpus_lda = lda_model[corpus_tfidf]
+perplexity = []
+coherence = []
+# トピック数を5ずつ増やしてLDAモデルを作成
+for N in range(5, 21, 5):  
+  # LDAを作成
+  lda = models.ldamodel.LdaModel(corpus_tfidf, num_topics=N, id2word=dictionary)
+  #corpus_lda = lda_model[corpus_tfidf]
 
-# モデルを保存
-lda_model.save(f'models/aozorabunko_{N}.model')
+  # モデルを保存
+  lda.save(f'/home/s192c1058/models/models_{N}.model')
 
-# トピックを出力
-for i in range(N):
-  print('TOPIC: ',i,'_',lda_model.print_topic(i))
+  # トピックを出力
+  lda_print_topics(lda)
+
+  # 評価指標を算出
+  pp, ch = compute_metrics(model=lda, texts=processed_corpus, corpus=corpus_tfidf, dct=dictionary)
+  perplexity.append(pp)
+  coherence.append(ch)
+
+  with open('ppch.txt', 'w') as f:
+    f.write(pp)
+    f.write(ch)
+
+# 評価指標をcsvに出力
+frame = {'perplexity': perplexity, 'coherence': coherence}
+df = pd.DataFrame(frame)
+df.to_csv('lda_evaluation.csv')
+
+  
